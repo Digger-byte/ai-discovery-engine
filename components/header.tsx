2c233798f3314c7ae36dd01c94d6e3e1cd90a8a1
Digger-byte <davidpmarshall67@gmail.com>
2025-04-02 19:34:01 +0100

    Create header.tsx

---
+++ components/header.tsx
@@ -0,0 +1,12 @@
+import { Logo } from "./logo"
+
+export function Header() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 items-center">
+        <Logo />
+        <div className="flex flex-1 items-center justify-end">{/* Add navigation or other header elements here */}</div>
+      </div>
+    </header>
+  )
+}
